fccb95d678462bf95f0a40a6839c6c5b324eca90
e-shekharreddy <shekharreddy.eanala.com>
2026-09-22 12:46:32 +0530

    CICD

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ instana({
     tracing: {
         enabled: true
     }
-});
-
+}); 
+// some cha nge
+// some changes, few more changes
 const { MongoClient, ObjectId } = require('mongodb');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -55,7 +56,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
-    var stat = {
+    const stat = {
         app: 'OK',
         mongo: mongoConnected
     };
@@ -181,3 +182,5 @@ const port = process.env.CATALOGUE_SERVER_PORT || '8080';
 app.listen(port, () => {
     logger.info('Started on port', port);
 });
+
+module.exports = app;
